147da49cd35921f8c8804b85260cf77ec85860c2
Anisha Sunkerneni <anishasunkerneni@Anishas-MacBook-Air.local>
2020-09-21 16:12:18 -0700

    update

--- pages/rules.js
+++ pages/rules.js
@@ -15,15 +15,15 @@ export default function Rules() {
         <section className="body h-auto" style={{ backgroundImage: "url('hero.png')" }}>
           <Navbar />
           <div className="p-6 flex flex-col justify-center items-center text-center">
-          <h1 className="text-white text-2xl md:text-4xl mt-20 md:mt-16">
+          <h1 className="text-white text-2xl md:text-4xl mt-20 md:mt-16 ">
               Sweepstakes Rules
           </h1>
           <div className="p-6 flex flex-col xl:flex-row lg:flex-col justify-center items-center text-center">
-            <h1 className="text-black hover:bg-orange-100 hover:text-white max-w-md bg-white text-xl md:text-xl mt-32 xl:mt-20 lg:mt-20 mx-10 p-6 md:mt-16 lg:mb-20 xl:mb-20">
-              <a href="/teslagiveaway">Up to Us x Cheeky Boyos</a>
+            <h1 className="mt-32 xl:mt-20 lg:mt-20 mx-10 md:mt-16 lg:mb-20 xl:mb-20">
+              <a className="text-black hover:bg-orange-100 hover:text-white max-w-md rounded-md p-6 bg-white text-xl md:text-xl " href="/teslagiveaway">Up to Us x Cheeky Boyos</a>
             </h1>
-            <h1 className="text-black hover:bg-orange-100 hover:text-white max-w-md bg-white text-xl md:text-xl mt-20 mb-8 lg:mb-20 xl:mb-20 lg:mt-20 xl:mt-20 mx-10 p-6 md:mt-16">
-              <a href="/checkandchat">Check and Chat Promotion</a>
+            <h1 className="mt-20 mb-8 lg:mb-20 xl:mb-20 lg:mt-20 xl:mt-20 mx-10 md:mt-16">
+              <a className="text-black hover:bg-orange-100 hover:text-white max-w-md rounded-md bg-white text-xl md:text-xl p-6 " href="/checkandchat">Check and Chat Promotion</a>
             </h1>
           </div>
           </div>
