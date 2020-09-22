a7cb4413c8e08d047c3116873e1c5453779e37ed
ndneighbor <15745278+ndneighbor@users.noreply.github.com>
2020-09-22 12:03:10 -0400

    map live

--- components/molecules/map.js
+++ components/molecules/map.js
@@ -44,14 +44,13 @@ export default class Map extends Component {
       earthquakes: null,
     };
 
-
     this._handleChangeDay = this._handleChangeDay.bind(this);
     this._handleChangeAllDay = this._handleChangeAllDay.bind(this);
   }
 
   componentDidMount() {
     requestJson(
-      "https://raw.githubusercontent.com/uptous2020/dataTransformation/master/citystatetransformed.json?token=ADYEB7W7NU52WYXM2KCBSBC7NGYM",
+      "https://raw.githubusercontent.com/uptous2020/dataTransformation/master/citystatetransformed.json?token=ADYEB7TQVMLLOJROSP6NKOS7NIPHU",
       (error, response) => {
         if (!error) {
           // Note: In a real application you would do a validation of JSON data before doing anything with it,
