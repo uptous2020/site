7100beda462e953d6c24f4645e98fb605668c181
Jimmy Hsu <jimmyhsu@live.com>
2020-09-22 00:02:42 -0700

    fix: round 2?

--- postcss.config.js
+++ postcss.config.js
@@ -4,14 +4,16 @@ module.exports = {
         autoprefixer: {},
         ...(process.env.NODE_ENV === 'production'
         ? {
-            '@fullhuman/postcss-purgecss': {
-                content: [
-                './components/**/*.js',
-                './pages/**/*.js'
-                ],
-                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-                safelist: ['bg-orange-600', 'bg-black']
-            }
+                '@fullhuman/postcss-purgecss': {
+                    content: [
+                        './components/**/*.js',
+                        './pages/**/*.js'
+                    ],
+                    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+                    safelist: {
+                        standard: [/orange$/, /black$/],
+                    }
+                }
             }
         : {})
     }
