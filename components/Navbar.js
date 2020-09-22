1e04357efd0cdae6756711bbeba882e9fe83f93d
Anisha Sunkerneni <anishasunkerneni@Anishas-MacBook-Air.local>
2020-09-22 02:33:40 -0700

    styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar() {
                 <div className="flex justify-center md:justify-between md:pr-20">
                     <img className="mt-3 w-8 h-12" src="/logowhite.svg" href="2020upto.us"></img>
                     <div>
-                        <a className="inline-block text-white text-3xl mr-12 md:mr-0 no-underline hover:text-gray-200 hover:text-underline py-6 px-1 "
+                        <a className="inline-block text-white text-3xl md:mr-0 no-underline hover:text-gray-200 hover:text-underline py-6 px-1 "
                            href="https://2020upto.us">up to us</a>
                     </div>
                 </div>
@@ -30,12 +30,27 @@ export default function Navbar() {
                     <ul className="self-center flex flex-row justify-evenly md:justify-between text-sm md:text-base">
                         <li>
                           <Link href="/about">
-                            <a className=" text-white no-underline md:ml-4 mr-2 md:mr-10 hover:text-gray-200 hover:text-underline">
+                            <a className=" text-white no-underline md:ml-4 mr-3 ml-10 md:mr-10 hover:text-gray-200 hover:text-underline">
                               ABOUT
                             </a>
                           </Link>
                         </li>
-                        {
+                        <li>
+                          <Link href="/team">
+                            <a className=" text-white no-underline md:ml-4 mr-3 md:mr-10 hover:text-gray-200 hover:text-underline">
+                              TEAM
+                            </a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/contact">
+                            <a className=" text-white no-underline md:ml-4 mr-3 md:mr-10 hover:text-gray-200 hover:text-underline">
+                              CONTACT
+                            </a>
+                          </Link>
+                        </li>
+                        
+                        {/* {
                             pages.filter((page) => {
                                 if (page == currentPage) {
                                     return false
@@ -50,7 +65,7 @@ export default function Navbar() {
                                     </Link>
                                 </li>)
                             )
-                        }
+                        } */}
                         <li className="text-orange-100 no-underline mr-10 mb-4 md:mr-0 hover:text-gray-200 hover:text-underline">
                             <script
                                 type="text/javascript"
