08c7ef8a1ae7b97d850428b1f46b90ed6e50896f
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-09-21 21:38:32 -0400

    added dynamic props

--- components/ContestLink.js
+++ components/ContestLink.js
@@ -2,21 +2,26 @@ import React from "react";
 import RightArrow from "./RightArrow.js"
 {/* /public/profiles/benji.jpg */}
 const ContestLink = ({
-  link,
-  description,
-    influencerPhoto,
-    bgColor = "black",
-    
-}) => { 
+  contestLink,
+  influencerPhoto,
+  bgColor = "black",
+  influencerName,
+}) => {
   return (
     <div className={`mx-4 bg-${bgColor} rounded-lg`}>
-    <div className="mx-4 my-2 flex flex-row items-center">
-      <img className="h-16 w-16 md:h-20 md:w-20 rounded-full m-2" src="profiles/benji.jpg" />
-      <p className="mx-2 w-64 mr-0 md:text-left text-white">
-        Win a Tesla from the Cheeky Boyos
-      </p>
-              <RightArrow color={bgColor == "black" ? "#E69538" : "#000000"}/>
-      </div>
+      <a href={contestLink}>
+        <div className="mx-4 my-2 flex flex-row items-center">
+          <img
+            className="h-16 w-16 md:h-20 md:w-20 rounded-full m-2"
+            src={influencerPhoto}
+          />
+
+          <p className="mx-2 w-64 mr-0 md:text-left text-white">
+            Win a chat with {influencerName}
+          </p>
+          <RightArrow color={bgColor == "black" ? "#E69538" : "#000000"} />
+        </div>
+      </a>
     </div>
   );
 };
