077a28b56b3c797534cbcd3c3a952d989f474d22
ndneighbor <15745278+ndneighbor@users.noreply.github.com>
2020-10-30 21:22:55 -0500

    upcoming changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,22 @@
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-        ...(process.env.NODE_ENV === 'production'
-        ? {
-                '@fullhuman/postcss-purgecss': {
-                    content: [
-                        './components/**/*.js',
-                        './pages/**/*.js'
-                    ],
-                    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-                    whitelist: {
-                        standard: [/orange$/, /black$/],
-                    }
-                }
-            }
-        : {})
-    }
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: [
+              "./components/**/*.js",
+              "./pages/**/*.js",
+              "./pages/**/*.mjs",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+            whitelist: {
+              standard: [/orange$/, /black$/],
+            },
+          },
+        }
+      : {}),
+  },
 };
