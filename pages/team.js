ac2faa94f94da737697dafc21a3a3e39be0dcdb2
ndneighbor <asara019@fiu.edu>
2021-03-30 23:35:19 -0400

    small fixes

--- pages/team.js
+++ pages/team.js
@@ -1,12 +1,10 @@
 import Head from "next/head";
 import data from "./teamcsv.json";
-
 import data2 from "./teamcsv2.json";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Link from "next/link";
 import { ArrowNarrowRightIcon } from "@heroicons/react/outline";
-
 
 const SocialLink = ({ link, icon, alt }) => (
   <div className="text-center py-2 m-2">
@@ -16,15 +14,13 @@ const SocialLink = ({ link, icon, alt }) => (
   </div>
 );
 
-
 const alts = ["LinkedIn", "Instagram", "TikTok", "GitHub", "Twitter"];
 const icons = [
   "linkedin_black.svg",
   "instagram_black.svg",
   "tiktok_black.svg",
   "github_black.svg",
-  "twitter_black.svg"
-
+  "twitter_black.svg",
 ];
 const profilePics = [
   "conor.jpg",
@@ -33,9 +29,8 @@ const profilePics = [
   "emilyds.jpg",
   "B.jpg",
   "alexr.jpg",
-
-  "coreywerner.jpg"
-].map(e => `profiles/${e.length ? e : "conor.jpg"}`);
+  "coreywerner.jpg",
+].map((e) => `profiles/${e.length ? e : "conor.jpg"}`);
 
 const profilePics2 = [
   "cullen.jpg",
@@ -55,54 +50,9 @@ const profilePics2 = [
   "anisha.jpg",
   "eshita.jpg",
   "ali.jpg",
-  "katie.jpg"
-].map(e => `profiles/${e.length ? e : "hanna.jpg"}`);
-
-const transformationFunction = [
-  link => link,
-  name => `https://www.instagram.com/${name}`,
-  name => `https://www.tiktok.com/${name}`,
-  name => `https://github.com/${name}`,
-  name => `https://twitter.com/${name}`
-];
-const components = alts.map((alt, i) => link => () => (
-  <SocialLink
-    link={transformationFunction[i](link)}
-    icon={icons[i]}
-    alt={alt}
-  />
-));
-
-const TeamMember = ({ teamObject, profilePic }) => {
-  const { Name, Role, Bio, Vaccine, Voting } = teamObject;
-  const SocialLinkComponent = alts
-    .map(e => [e, teamObject[e]])
-    .filter(tuple => tuple[0].length)
-    .map(([alt, link]) => components[alts.indexOf(alt)](link));
-  return (
-    <div className="mb-8 flex flex-col items-center">
-      <div className="w-40 h-40 m-4">
-        <img src={profilePic} className="flex text-center" Image />
-      </div>
-=======
-  "hanna.jpg",
-  "shreya.jpg",
-  "alex.jpg",
-  "josh_svoboda_square.jpg",
-  "lindseykennedy.jpg",
-  "rosendo.jpg",
-  "shianne.jpg",
-  "angelo.webp",
-  "Jimbo.jpg",
-  "Long.jpg",
-  "benji.jpg",
-  "aaron.jpg",
-  "nik.jpg",
-  "anisha.jpg",
-  "eshita.jpg",
-  "ali.jpg",
   "katie.jpg",
-].map((e) => `profiles/${e.length ? e : "conor.jpg"}`);
+].map((e) => `profiles/${e.length ? e : "hanna.jpg"}`);
+
 const transformationFunction = [
   (link) => link,
   (name) => `https://www.instagram.com/${name}`,
@@ -119,18 +69,16 @@ const components = alts.map((alt, i) => (link) => () => (
 ));
 
 const TeamMember = ({ teamObject, profilePic }) => {
-  const { Name, Role, Bio } = teamObject;
+  const { Name, Role, Bio, Vaccine, Voting } = teamObject;
   const SocialLinkComponent = alts
     .map((e) => [e, teamObject[e]])
-    .filter((tuple) => tuple[1].length)
+    .filter((tuple) => tuple[0].length)
     .map(([alt, link]) => components[alts.indexOf(alt)](link));
   return (
-    <div className="flex flex-col items-center">
+    <div className="mb-8 flex flex-col items-center">
       <div className="w-40 h-40 m-4">
         <img src={profilePic} className="flex text-center" Image />
       </div>
-
-
       <h2 className="">{Name}</h2>
       <h3 className="mb-4">{Role}</h3>
       <p className="mb-1 w-3/5 text-center">{Bio}</p>
@@ -139,19 +87,18 @@ const TeamMember = ({ teamObject, profilePic }) => {
           <SocialLink key={i} />
         ))}
       </div>
-
-      <div className="flex space-x-4 justify-center mb-4">
+      <div className="flex mx-4 justify-between mb-4 p-2">
         {Vaccine === "" || Vaccine === undefined ? (
           <div></div>
         ) : (
-          <div className="mx-2 text-center h-7 w-32 container mx-auto ml-8 border-2 border-black rounded">
+          <div className="mx-2 mr-8 text-center h-7 w-32 container mx-auto border-2 border-black rounded">
             <p className="py-2">{Vaccine}</p>
           </div>
         )}
         {Voting === "" || Voting === undefined ? (
           <div></div>
         ) : (
-          <div className="text-center h-7 w-32 container mx-auto ml-8 border-2 border-black rounded">
+          <div className="mx-2 text-center h-7 w-32 container mx-auto border-2 border-black rounded">
             <p className="py-2">{Voting}</p>
           </div>
         )}
@@ -171,9 +118,7 @@ export default function About() {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-
-          })(window,document,'script','dataLayer','GTM-WPBV7FT');`
-
+          })(window,document,'script','dataLayer','GTM-WPBV7FT');`,
           }}
         />
         <script
@@ -187,7 +132,7 @@ export default function About() {
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
                   gtag('config', 'UA-178646695-1');
-              `
+              `,
           }}
         />
         <title>Up To Us</title>
@@ -202,14 +147,12 @@ export default function About() {
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WPBV7FT"
-
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />
           <Navbar />
           <div className="p-6 text-white flex flex-col items-center text-center mb-20">
             <h1 className="text-white text-2xl md:text-6xl mt-20 md:mt-16 pt-10 pb-10">
-
               TEAM
             </h1>
           </div>
@@ -241,13 +184,11 @@ export default function About() {
             CONTRIBUTORS
           </h1>
           <div className="mx-auto sm:grid grid-cols-1 w-full md: grid-cols-2 w-2/3">
-
             {data2.map((member, i) => (
               <TeamMember
                 key={i}
                 teamObject={member}
                 profilePic={profilePics2[i]}
-
               />
             ))}
           </div>
